c349ccc322d26f47106e2b1791002020d0b710b8
Adam J. Richardson <fatman.uk@gmail.com>
2024-03-10 00:32:05 +0000

    Add various comment and variable tweaks

--- scripts/virsh.bash
+++ scripts/virsh.bash
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 ACTION="$1"
 DOMAIN="$2"
-HOSTS='k3s-mother-001 k3s-mother-002 k3s-mother-003 k3s-child-001 k3s-child-002 k3s-child-003 k3s-child-004 k3s-child-005 k3s-child-006 k3s-child-007 k3s-child-008 k3s-child-009 rancher-admin'
+HOSTS='k3s-mother-01 k3s-mother-02 k3s-mother-03 k3s-child-01 k3s-child-02 k3s-child-03 k3s-child-04 k3s-child-05 k3s-child-06 rancher-admin'
 for HOST in ${HOSTS}; do
 	virsh ${ACTION} ${HOST}.${DOMAIN}
 done
